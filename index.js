bad84874c84445053a8b07f7c08baa9cbf3b7a67
Hridhyapaul <hpdesign2021@gmail.com>
2023-05-02 01:01:35 +0600

    recipes route created

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const app = express()
 const port = 3000
 
 const chef = require('./Data/chef.json')
+const recipe = require('./Data/Recipe.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/chefs', (req, res) => {
     res.send(chef)
-  })
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipe)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
