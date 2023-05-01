e8e5f98bdd7b397083e707c82d4c27145438675c
Hridhyapaul <hpdesign2021@gmail.com>
2023-05-02 02:35:46 +0600

    Server site deployed in Vercel

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
-
+const port = process.env.PORT || 3000;
 const chef = require('./Data/chef.json')
 const recipe = require('./Data/Recipe.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,6 +17,18 @@ app.get('/chefs', (req, res) => {
 
 app.get('/recipes', (req, res) => {
     res.send(recipe)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chefsDetails = recipe.find(r => r.chefs_id === id)
+    res.send(chefsDetails)
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    const recipesDetails = recipe.find(r => r.id === id)
+    res.send(recipesDetails)
 })
 
 app.listen(port, () => {
